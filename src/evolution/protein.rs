4d55166251487ca65ff552275e2c83ee9edae5cd
at-robins <30956028+at-robins@users.noreply.github.com>
2019-11-19 20:58:31 +0100

    Added Value Setter For Substrate
    A setter was added for Substrate. Some documentation was added.

--- src/evolution/protein.rs
+++ src/evolution/protein.rs
@@ -2,6 +2,7 @@ extern crate bitvec;
 
 use std::rc::Rc;
 use bitvec::boxed::BitBox;
+use super::chemistry::Reaction;
 
 /// A `Substrate` represents a chemical entity of a specific value. Upon change
 /// [`Receptor`]s of the substrate will be notified.
@@ -12,15 +13,40 @@ pub struct Substrate {
     receptors: Vec<Rc<Receptor>>,
 }
 
+impl Substrate {
+    /// Set the value of this substrate.alloc
+    /// 
+    /// # Parameters
+    /// 
+    /// * `value` - the new value of the substrate
+    pub fn set_value(&mut self, value: BitBox) {
+        self.value = value;
+    }
+}
+
 /// A `Receptor` is a sensor for [`Substrate`] changes and a trigger
 /// for [`Reaction`]s.
 /// 
 /// [`Substrate`]: ./struct.Substrate.html 
 /// [`Reaction`]: ../chemistry/struct.Substrate.html 
 pub struct Receptor {
-    
+    substrates: Vec<Rc<Substrate>>,
+    enzym: Protein,
 }
 
+/// A `Protein` produces products from educt [`Substrate`]s 
+/// by performing a [`Reaction`].
+/// 
+/// [`Substrate`]: ./struct.Substrate.html 
+/// [`Reaction`]: ../chemistry/struct.Substrate.html 
 pub struct Protein {
-    
+    educts: Vec<Rc<Substrate>>,
+    products: Vec<Rc<Substrate>>,
+    reaction: Reaction,
 }
+
+/*impl Protein {
+    pub fn catalyse(&self) {
+        self.reaction.react();
+    }
+}*/
