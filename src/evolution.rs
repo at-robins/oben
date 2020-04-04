b9e135504050f765ed96e7652b14f7f76b610c94
at-robins <30956028+at-robins@users.noreply.github.com>
2020-04-04 20:02:00 +0200

    Structure For Genomic Classes
    Added a module with information containing structures, which will be 
    translated into the executive  protein structures.

--- src/evolution.rs
+++ src/evolution.rs
@@ -1,2 +1,3 @@
 pub mod chemistry;
 pub mod protein;
+pub mod gene;
---
+++ src/evolution/gene.rs
@@ -0,0 +1,66 @@
+//! The `gene` module contains information structures to describe,
+//! replicate and mutate the evolutionary network.
+extern crate bitvec;
+extern crate rand;
+
+use super::chemistry::{Reaction, State};
+use bitvec::{boxed::BitBox};
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+pub struct Gene {
+    substrates: Vec<BitBox>,
+    input: Vec<usize>,
+    output: Vec<usize>,
+    receptors: Vec<GenomicReceptor>,
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+pub struct GenomicReceptor {
+    state: State,
+    enzyme: GenomicCatalyticCentre,
+}
+
+/// A `GenomicCatalyticCentre` represents the information of an actual [`CatalyticCentre`] that
+/// produces products from educt [`Substrate`]s
+/// by performing a [`Reaction`]. It is contained within a [`Gene`].
+///
+/// [`CatalyticCentre`]: ../protein/struct.CatalyticCentre.html
+/// [`Gene`]: ./struct.Gene.html
+/// [`Substrate`]: ./struct.Substrate.html
+/// [`Reaction`]: ../chemistry/struct.Reaction.html
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+pub struct GenomicCatalyticCentre {
+    educts: Vec<usize>,
+    products: Vec<usize>,
+    reaction: Reaction,
+}
+
+impl GenomicCatalyticCentre {
+    /// Creates a new `GenomicCatalyticCentre` containing the information to produce
+    /// an actual [`CatalyticCentre`] via the containing [`Gene`].
+    ///
+    /// # Parameters
+    ///
+    /// * `educts` - the educt [`Substrate`]s index in the respective containing [`Gene`]
+    /// * `products` - the product [`Substrate`]s index in the respective containing [`Gene`]
+    /// * `reaction` - the [`Reaction`] to catalyse
+    ///
+    /// # Panics
+    ///
+    /// If the number of educt or product [`Substrate`]s is not exactly equal to the one
+    /// required by the [`Reaction`].
+    ///
+    /// [`CatalyticCentre`]: ../protein/struct.CatalyticCentre.html
+    /// [`Gene`]: ./struct.Gene.html
+    /// [`Substrate`]: ../protein/struct.Substrate.html
+    /// [`Reaction`]: ../chemistry/struct.Reaction.html
+    pub fn new(educts: Vec<usize>, products: Vec<usize>, reaction: Reaction) -> Self {
+        assert_eq!(educts.len(), reaction.get_educt_number(),
+            "The number of required educts for reaction {:?} is {}, but {} educts were supplied.",
+            reaction, reaction.get_educt_number(), educts.len());
+        assert_eq!(products.len(), reaction.get_product_number(),
+            "The number of required products for reaction {:?} is {}, but {} products were supplied.",
+            reaction, reaction.get_product_number(), products.len());
+        GenomicCatalyticCentre{educts, products, reaction}
+    }
+}
