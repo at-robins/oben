1fa4e8069a007634d3750434fbed8b2d85b78516
at-robins <30956028+at-robins@users.noreply.github.com>
2020-06-26 09:18:16 +0200

    Added Environment Builder
    An environment builder was implemented for convenience.

--- src/evolution/environment.rs
+++ src/evolution/environment.rs
@@ -27,6 +27,270 @@ const FILE_EXTENSION_POPULATION: &str = "population";
 /// The context for UUID creation.
 const UUID_CONTEXT: Context = Context::new(0);
 
+/// An `EnvironmentBuilder` specifing settings for an evolutionary network to develop in and
+/// returning the corresponding [`Environment`].
+///
+/// [`Environment`]: ./struct.Environment.html
+#[derive(Debug, PartialEq, Clone)]
+pub struct EnvironmentBuilder {
+    working_directory: PathBuf,
+    /// The chance of a single offspring to carry a mutation.
+    mutation_rate: Option<f64>,
+    /// The maximum size a [`ClonalPopulation`] can grow to.
+    ///
+    /// [`ClonalPopulation`]: ../population/struct.ClonalPopulation.html
+    population_size: u32,
+    /// The relative size of clonal sub-populations that can still be detected.
+    /// Any population with a smaller size will go extinct.
+    extinction_threshold: Option<f64>,
+    /// The amount of time an [`Organism`] of a [`ClonalPopulation`] has to complete a task.
+    ///
+    /// [`ClonalPopulation`]: ../population/struct.ClonalPopulation.html
+    /// [`Organism`]: ../population/struct.Organism.html
+    lifespan: Duration,
+    /// The time intervall in which to save the current [`Population`] to a file.
+    ///
+    /// [`Population`]: ../population/struct.Population.html
+    population_save_intervall: Duration,
+    /// The node for UUID creation.
+    uuid_node: [u8; 6],
+    /// The maximum age of a [`ClonalPopulation`] until testing and fitness determination is
+    /// always performed. After this age testing is performed by chance.
+    ///
+    /// [`ClonalPopulation`]: ../population/struct.ClonalPopulation.html
+    max_testing_age: Option<u32>,
+    /// The size of a newly founded, mutated [`ClonalPopulation`].
+    ///
+    /// [`ClonalPopulation`]: ../population/struct.ClonalPopulation.html
+    clonal_population_founding_size: Option<f64>,
+    /// The standard deviation of the fitness dependent growth of a [`ClonalPopulation`].
+    /// It is specified in percentage of the sub-population's size.
+    ///
+    /// [`ClonalPopulation`]: ../population/struct.ClonalPopulation.html
+    clonal_population_growth_sd: f64,
+    /// The chance per growth event that a lateral gene transfer between two
+    /// sub-populations happens.
+    lateral_gene_transfer_chance: Option<f64>,
+    /// The 50 percent midpoint of test cycles of the chance determining sigmoid for testing a [`ClonalPopulation`].
+    ///
+    /// [`ClonalPopulation`]: ../population/struct.ClonalPopulation.html
+    testing_chance_sigmoid_midpoint: f64,
+}
+
+impl EnvironmentBuilder {
+
+    pub fn new() -> Self {
+        EnvironmentBuilder {
+            working_directory: PathBuf::from("./working_directory"),
+            mutation_rate: None,
+            population_size: 1_000_000,
+            extinction_threshold: None,
+            lifespan: Duration::from_secs(1),
+            population_save_intervall: Duration::from_secs(1800),
+            uuid_node: rand::random(),
+            max_testing_age: None,
+            clonal_population_founding_size: None,
+            clonal_population_growth_sd: 0.1,
+            lateral_gene_transfer_chance: None,
+            testing_chance_sigmoid_midpoint: 50.0
+        }
+    }
+
+    /// Build an [`Environment`] to specify run time properties of an evolutionary network.
+    pub fn build(&self) -> Environment {
+        Environment {
+            working_directory: self.working_directory.clone(),
+            mutation_rate: self.mutation_rate_or_default(),
+            population_size: self.population_size,
+            extinction_threshold: self.extinction_threshold_or_default(),
+            lifespan: self.lifespan,
+            population_save_intervall: self.population_save_intervall,
+            uuid_node: self.uuid_node,
+            max_testing_age: self.max_testing_age,
+            clonal_population_founding_size: self.clonal_population_founding_size_or_default(),
+            clonal_population_growth_sd: self.clonal_population_growth_sd,
+            lateral_gene_transfer_chance: self.lateral_gene_transfer_chance_or_default(),
+            testing_chance_sigmoid_midpoint: self.testing_chance_sigmoid_midpoint
+        }
+    }
+
+    /// Sets the working directory as specified.
+    ///
+    /// # Parameters
+    ///
+    /// * `working_directory` - the working directory
+    pub fn working_directory<P: AsRef<Path>>(&mut self, working_directory: P) -> &mut Self {
+        self.working_directory = working_directory.as_ref().into();
+        self
+    }
+
+    /// Sets the mutation rate as specified.
+    ///
+    /// # Parameters
+    ///
+    /// * `mutation_rate` - the chance of a single mutation occuring
+    pub fn mutation_rate(&mut self, mutation_rate: f64) -> &mut Self {
+        self.mutation_rate = Some(mutation_rate);
+        self
+    }
+
+    /// Sets the population size as specified.
+    ///
+    /// # Parameters
+    ///
+    /// * `population_size` - the maximum size of the total population
+    pub fn population_size(&mut self, population_size: u32) -> &mut Self {
+        self.population_size = population_size;
+        self
+    }
+
+    /// Sets the extinction thresholdas as specified.
+    ///
+    /// # Parameters
+    ///
+    /// * `extinction_threshold` - the minimum relative size of a sub-population before being
+    /// no longer detectable and thereby going extinct
+    pub fn extinction_threshold(&mut self, extinction_threshold: f64) -> &mut Self {
+        self.extinction_threshold = Some(extinction_threshold);
+        self
+    }
+
+    /// Sets the maximum run time of a sub-population per task as specified.
+    ///
+    /// # Parameters
+    ///
+    /// * `lifespan` - the maximum run time per tested task
+    pub fn lifespan(&mut self, lifespan: Duration) -> &mut Self {
+        self.lifespan = lifespan;
+        self
+    }
+
+    /// Sets the intervall in which the total population is saved to disk as specified.
+    /// The actual saving intervall might be longer as saving is only performed after each
+    /// generation.
+    ///
+    /// # Parameters
+    ///
+    /// * `population_save_intervall` - the save intervall
+    pub fn population_save_intervall(&mut self, population_save_intervall: Duration) -> &mut Self {
+        self.population_save_intervall = population_save_intervall;
+        self
+    }
+
+    /// Sets the UUID node as specified.
+    ///
+    /// # Parameters
+    ///
+    /// * `uuid_node` - the node for UUID generation
+    pub fn uuid_node(&mut self, uuid_node: [u8; 6]) -> &mut Self {
+        self.uuid_node = uuid_node;
+        self
+    }
+
+    /// Sets the number of times testing of a sub-population is mandatorily performed as specified.
+    /// After this threshold is reached, testing is performed on a statistical basis.
+    /// If `None` every sub-population is tested in every generation.
+    ///
+    /// # Parameters
+    ///
+    /// * `max_testing_age` - the maximum mandatory testing age
+    pub fn max_testing_age(&mut self, max_testing_age: Option<u32>) -> &mut Self {
+        self.max_testing_age = max_testing_age;
+        self
+    }
+
+    /// Sets the relative size of newly found populations as specified.
+    ///
+    /// # Parameters
+    ///
+    /// * `clonal_population_founding_size` - the initial size of sub-populations
+    pub fn clonal_population_founding_size(&mut self, clonal_population_founding_size: f64) -> &mut Self {
+        self.clonal_population_founding_size = Some(clonal_population_founding_size);
+        self
+    }
+
+    /// Sets the standard deviation for growth of sub-populations as specified.
+    /// The standard deviation is specified in percentage of relative sub-population size.
+    ///
+    /// # Parameters
+    ///
+    /// * `clonal_population_growth_sd` - the growth standard deviation
+    pub fn clonal_population_growth_sd(&mut self, clonal_population_growth_sd: f64) -> &mut Self {
+        self.clonal_population_growth_sd = clonal_population_growth_sd;
+        self
+    }
+
+    /// Sets the chance of lateral gene transfer as specified.
+    ///
+    /// # Parameters
+    ///
+    /// * `lateral_gene_transfer_chance` - the chance of lateral gene transfer
+    pub fn lateral_gene_transfer_chance(&mut self, lateral_gene_transfer_chance: f64) -> &mut Self {
+        self.lateral_gene_transfer_chance = Some(lateral_gene_transfer_chance);
+        self
+    }
+
+    /// Sets the midpoint of the sigmoid determining the chance of statistical testing as specified.
+    /// This is the number of tests for a sub-populations where the chance of testing
+    /// per generation is 50 percent.
+    ///
+    /// # Parameters
+    ///
+    /// * `testing_chance_sigmoid_midpoint` - the midpoint of the testing chance sigmoid
+    pub fn testing_chance_sigmoid_midpoint(&mut self, testing_chance_sigmoid_midpoint: f64) -> &mut Self {
+        self.testing_chance_sigmoid_midpoint = testing_chance_sigmoid_midpoint;
+        self
+    }
+
+    /// Returns the mutation rate if set. Otherwise defaults to a population size dependent value.
+    fn mutation_rate_or_default(&self) -> f64 {
+        if let Some(mutation_rate) = self.mutation_rate {
+            mutation_rate
+        } else {
+            // Defaults to around 1000 mutations per generation.
+            1000.0 / self.population_size as f64
+        }
+    }
+
+    /// Returns the extinction threshold if set. Otherwise defaults to a population size dependent value.
+    fn extinction_threshold_or_default(&self) -> f64 {
+        if let Some(extinction_threshold) = self.extinction_threshold {
+            extinction_threshold
+        } else {
+            // Defaults to a bit less than one individual.
+            0.3 / self.population_size as f64
+        }
+    }
+
+    /// Returns the clonal population founding size if set.
+    /// Otherwise defaults to a population size dependent value.
+    fn clonal_population_founding_size_or_default(&self) -> f64 {
+        if let Some(clonal_population_founding_size) = self.clonal_population_founding_size {
+            clonal_population_founding_size
+        } else {
+            // Defaults to a bit more than one individual.
+            3.0 / self.population_size as f64
+        }
+    }
+
+    /// Returns the chance of lateral gene transfer if set.
+    /// Otherwise defaults to a population size dependent value.
+    fn lateral_gene_transfer_chance_or_default(&self) -> f64 {
+        if let Some(lateral_gene_transfer_chance) = self.lateral_gene_transfer_chance {
+            lateral_gene_transfer_chance
+        } else {
+            // Defaults to around 10 events per generation.
+            10.0 / self.population_size as f64
+        }
+    }
+}
+
+impl Default for EnvironmentBuilder {
+    fn default() -> Self {
+        EnvironmentBuilder::new()
+    }
+}
+
 /// An `Environment` specifing settings for an evolutionary network to develop in.
 #[derive(Debug, PartialEq, Clone)]
 pub struct Environment {
@@ -75,33 +339,6 @@ pub struct Environment {
 }
 
 impl Environment {
-    // TODO: Implement an environment builder.
-    pub fn new(working_directory: PathBuf,
-        mutation_rate: f64,
-        population_size: u32,
-        extinction_threshold: f64,
-        lifespan: Duration,
-        population_save_intervall: Duration,
-        uuid_node: [u8; 6],
-        max_testing_age: Option<u32>,
-        clonal_population_founding_size: f64,
-        clonal_population_growth_sd: f64,
-        lateral_gene_transfer_chance: f64,
-        testing_chance_sigmoid_midpoint: f64) -> Self {
-        Environment{working_directory,
-            mutation_rate,
-            population_size,
-            extinction_threshold,
-            lifespan,
-            population_save_intervall,
-            uuid_node,
-            max_testing_age,
-            clonal_population_founding_size,
-            clonal_population_growth_sd,
-            lateral_gene_transfer_chance,
-            testing_chance_sigmoid_midpoint}
-    }
-
     /// Returns the path to the working directory.
     pub fn working_directory(&self) -> &Path {
         Path::new(&self.working_directory)
@@ -119,7 +356,7 @@ impl Environment {
         self.population_size
     }
 
-    /// Returns the threshold of relative populaion size which is still detectable before
+    /// Returns the threshold of relative population size which is still detectable before
     /// a [`ClonalPopulation`] goes extinct.
     ///
     /// [`ClonalPopulation`]: ../population/struct.ClonalPopulation.html
@@ -287,20 +524,13 @@ impl Environment {
 
 impl Default for Environment {
     fn default() -> Self {
-        Environment {
-            working_directory: PathBuf::from("./working_directory"),
-            mutation_rate: 0.0001,
-            population_size: 1_000_000,
-            extinction_threshold: 1.0 / 10_000_000.0,
-            lifespan: Duration::from_secs(1),
-            population_save_intervall: Duration::from_secs(1800),
-            uuid_node: rand::random(),
-            max_testing_age: None,
-            clonal_population_founding_size: 1.0 / 1_000_000.0,
-            clonal_population_growth_sd: 0.1,
-            lateral_gene_transfer_chance: 0.00001,
-            testing_chance_sigmoid_midpoint: 100.0
-        }
+        EnvironmentBuilder::new().build()
+    }
+}
+
+impl<E> From<E> for Environment where E: AsRef<EnvironmentBuilder>{
+    fn from(builder: E) -> Self {
+        builder.as_ref().build()
     }
 }
 
@@ -410,7 +640,7 @@ impl<I: 'static> GlobalEnvironment<I> {
             fitness = clonal_population.lock().unwrap().fitness().expect("The sub-population should have been tested before.");
         }
         let mutated_offspring = Self::get_mutated_offspring(clonal_population.clone(), fitness, inner.clone(), tested);
-        // Add the mutated offspring to the popuation.
+        // Add the mutated offspring to the population.
         inner.append_population(mutated_offspring);
     }
 
@@ -453,7 +683,7 @@ struct InnerGlobalEnvironment<I> {
 
 impl<I> InnerGlobalEnvironment<I> {
     /// Checks if the specified [`ClonalPopulation`] is already extinct.
-    /// A populaion cannot go extinct before its fitness was determined.
+    /// A population cannot go extinct before its fitness was determined.
     ///
     /// # Parameters
     ///
@@ -704,7 +934,7 @@ impl<I> InnerGlobalEnvironment<I> {
     /// [`ClonalPopulation`]: ../population/struct.ClonalPopulation.html
     fn load_organism(&self, clonal_population: Arc<Mutex<ClonalPopulation>>) -> Organism {
         clonal_population.lock()
-            .expect("Another thread panicked while holding the clonal popuation lock.")
+            .expect("Another thread panicked while holding the clonal population lock.")
             .genome()
             .translate()
     }
