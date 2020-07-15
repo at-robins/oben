e613f5266db36145338b808e3e5f1e98f2605fdc
at-robins <30956028+at-robins@users.noreply.github.com>
2020-07-16 00:00:53 +0200

    Marker Commit: Start Of The Rewrite
    A rewrite will be performed, switching from a population based model of 
    asexual replication to an individual based one with sexual reproduction 
    and degenerate genetic code.

--- src/evolution/binary.rs
+++ src/evolution/binary.rs
@@ -8,3 +8,39 @@ use bitvec::{boxed::BitBox, order::Msb0};
 ///
 /// [`Substrate`]: ../protein/struct.Substrate.html
 pub type BinarySubstrate = BitBox<Msb0, u8>;
+
+/// Converts the [`Substrate`] into a 64 bit array.
+///
+/// # Parameters
+///
+/// * `substrate` - the [`Substrate`] to convert
+///
+/// # Panics
+///
+/// If the [`Substrate`] is not of length `64`.
+///
+/// [`Substrate`]: ../protein/struct.Substrate.html
+pub fn as_64(substrate: &BinarySubstrate) -> [u8; 8] {
+    if substrate.len() != 64 {
+        panic!("64 bits are required for conversion, but {} were passed.", substrate.len());
+    }
+    let mut array = [0; 8];
+    let bytes = &substrate.as_slice()[..array.len()];
+    array.copy_from_slice(bytes);
+    array
+}
+
+/// Converts the [`Substrate`] into a 64 bit float.
+///
+/// # Parameters
+///
+/// * `substrate` - the [`Substrate`] to convert
+///
+/// # Panics
+///
+/// If the [`Substrate`] is not of length `64`.
+///
+/// [`Substrate`]: ../protein/struct.Substrate.html
+pub fn as_f64(substrate: &BinarySubstrate) -> f64 {
+    f64::from_be_bytes(as_64(substrate))
+}
