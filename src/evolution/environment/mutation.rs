443491ce84da118b52a02a654a70d4744661fbe8
at-robins <30956028+at-robins@users.noreply.github.com>
2022-01-06 11:14:00 +0100

    [10] Implemented mutation lists
    Mutation lists were implemented allowing mutations with different frequencies to be applied during the same genome duplication process.

--- src/evolution/environment/mutation.rs
+++ src/evolution/environment/mutation.rs
@@ -1,5 +1,8 @@
 //! The `mutation` module contains structures relevant for mutating genomes.
 
+use std::iter;
+
+use rand::{prelude::SliceRandom, thread_rng, Rng};
 use serde::{de::DeserializeOwned, Serialize};
 
 use crate::evolution::{
@@ -8,6 +11,11 @@ use crate::evolution::{
     helper::Nlbf64,
 };
 
+/// The maximum number of mutation events that might occur
+/// for a single [`Mutation`] type upon [`Genome`] duplication.
+const MAX_MUTATION_EVENTS: usize = 128;
+
+#[derive(Debug, PartialEq)]
 /// A list of [`Mutation`]s.
 pub struct MutationCompendium<
     ReactionType,
@@ -49,6 +57,16 @@ impl<
         }
     }
 
+    /// Returns the number of different mutations contained within the compendium.
+    pub fn size(&self) -> usize {
+        self.mutations.len()
+    }
+
+    /// Adds a [`Mutation`] to the list.
+    ///
+    /// # Parameters
+    ///
+    /// * `mutation` - the mutation to add to the compendium
     pub fn add(
         &mut self,
         mutation: Mutation<
@@ -60,6 +78,94 @@ impl<
         >,
     ) {
         self.mutations.push(mutation);
+    }
+
+    /// Mutates a [`Genome`] based on the [`Mutation`]s and
+    /// their respective mutation frequencies and returns
+    /// the mutated [`Genome`] if any [`Mutation`] occured
+    /// and it is still in a valid state.
+    ///
+    /// # Parameters
+    ///
+    /// * `genome` - the [`Genome`] to mutate
+    pub fn mutate(
+        &self,
+        genome: &Genome<
+            ReactionType,
+            StateType,
+            InformationType,
+            InputElementType,
+            InputSensorType,
+        >,
+    ) -> Option<Genome<ReactionType, StateType, InformationType, InputElementType, InputSensorType>>
+    {
+        let mut applied_mutations: Vec<
+            &Mutation<ReactionType, StateType, InformationType, InputElementType, InputSensorType>,
+        > = self
+            .mutations
+            .iter()
+            .flat_map(|mutation| iter::repeat(mutation).take(mutation.number_of_mutation_events()))
+            .collect();
+        // Randomise the order in which mutations are applied.
+        applied_mutations.shuffle(&mut thread_rng());
+        let mut mutated_genome = None;
+        for (i, mutation) in applied_mutations.into_iter().enumerate() {
+            if i == 0 {
+                // If it is the first mutation use the original genome.
+                mutated_genome = mutation.mutate(genome);
+            } else if let Some(g) = mutated_genome {
+                // On any subsequent mutation use the mutated genome.
+                mutated_genome = mutation.mutate(&g);
+            } else {
+                // If any of the mutations was not successful the rest of the loop can be skipped.
+                break;
+            }
+        }
+        mutated_genome
+    }
+}
+
+impl<ReactionType, StateType, InformationType, InputElementType, InputSensorType>
+    From<Vec<Mutation<ReactionType, StateType, InformationType, InputElementType, InputSensorType>>>
+    for MutationCompendium<
+        ReactionType,
+        StateType,
+        InformationType,
+        InputElementType,
+        InputSensorType,
+    >
+{
+    fn from(
+        mutations: Vec<
+            Mutation<ReactionType, StateType, InformationType, InputElementType, InputSensorType>,
+        >,
+    ) -> Self {
+        MutationCompendium { mutations }
+    }
+}
+
+impl<ReactionType, StateType, InformationType, InputElementType, InputSensorType>
+    From<
+        MutationCompendium<
+            ReactionType,
+            StateType,
+            InformationType,
+            InputElementType,
+            InputSensorType,
+        >,
+    >
+    for Vec<Mutation<ReactionType, StateType, InformationType, InputElementType, InputSensorType>>
+{
+    fn from(
+        compendium: MutationCompendium<
+            ReactionType,
+            StateType,
+            InformationType,
+            InputElementType,
+            InputSensorType,
+        >,
+    ) -> Self {
+        compendium.mutations
     }
 }
 
@@ -97,7 +203,7 @@ impl<
     ///
     /// # Parameters
     ///
-    /// * `mutation_chance` - the chance of the mutation happening during genome duplication
+    /// * `mutation_chance` - the chance of the mutation happening a single time during genome duplication
     /// * `mutation` - the underlying mutation function
     pub fn new<
         N: Into<Nlbf64>,
@@ -118,7 +224,7 @@ impl<
         }
     }
 
-    /// Returns the chance of this mutation happening during [`Genome`] duplication.
+    /// Returns the chance of this mutation happening during [`Genome`] duplication a single time.
     pub fn mutation_chance(&self) -> Nlbf64 {
         self.chance
     }
@@ -142,6 +248,31 @@ impl<
     ) -> Option<Genome<ReactionType, StateType, InformationType, InputElementType, InputSensorType>>
     {
         (self.mutation)(genome)
+    }
+
+    /// Simulates how often a mutation event occurs on a statistical basis.
+    /// This is an internal function with a random element.
+    fn number_of_mutation_events(&self) -> usize {
+        if self.chance == Nlbf64::MIN {
+            0
+        } else if self.chance == Nlbf64::MAX {
+            MAX_MUTATION_EVENTS
+        } else {
+            let random_chance: f64 = thread_rng().gen_range(0.0..1.0);
+            // Calculate the number of mutation events per mutation
+            // corresponding to the generated uniform random percentage.
+            //    P("n mutations in a single genome") = "mutation rate" ^ n
+            // => n = log(base: "mutation rate", value: P)
+            let number_of_mutations =
+                random_chance.log(self.mutation_chance().value()).floor() as usize;
+            // Limits the number of mutation events per mutation to
+            // prevent overflow in rare statistical cases.
+            if number_of_mutations > MAX_MUTATION_EVENTS {
+                MAX_MUTATION_EVENTS
+            } else {
+                number_of_mutations
+            }
+        }
     }
 }
 
--- src/evolution/environment/mutation/tests.rs
+++ src/evolution/environment/mutation/tests.rs
@@ -1,6 +1,9 @@
 use crate::evolution::{
     gene::{Gene, GeneSubstrate, GenomicInputSensor},
-    helper::testing::{TestGenome, TestInformation},
+    helper::{
+        noop::NoOpInputElement,
+        testing::{TestGenome, TestInformation, TestInput, TestReaction, TestState},
+    },
 };
 
 use super::*;
@@ -54,4 +57,181 @@ fn test_mutation_mutate() {
     let mutated_genome = mutation.mutate(&unmutated_genome);
     assert_eq!(Some(expected_mutated_genome), mutated_genome);
     assert_ne!(Some(unmutated_genome), mutated_genome);
+}
+
+#[test]
+/// Tests if the function `test_number_of_mutation_events` of the [`Mutation`] struct correctly simulates the mutation process.
+fn test_number_of_mutation_events() {
+    {
+        let chance = 0.0;
+        let mutation = Mutation::new(chance, |genome: &TestGenome| Some(genome.duplicate()));
+        assert_eq!(mutation.number_of_mutation_events(), 0);
+    }
+    {
+        let chance = 1.0;
+        let mutation = Mutation::new(chance, |genome: &TestGenome| Some(genome.duplicate()));
+        assert_eq!(mutation.number_of_mutation_events(), MAX_MUTATION_EVENTS);
+    }
+    {
+        let chance = 0.05;
+        let mutation = Mutation::new(chance, |genome: &TestGenome| Some(genome.duplicate()));
+        let n = mutation.number_of_mutation_events();
+        assert!(n <= MAX_MUTATION_EVENTS);
+    }
+}
+
+#[test]
+/// Tests if the function `new` of the [`MutationCompendium`] struct correctly creates an empty [`MutationCompendium`].
+fn test_mutation_compendium_new() {
+    let compendium: MutationCompendium<
+        TestReaction,
+        TestState,
+        TestInformation,
+        NoOpInputElement,
+        TestInput,
+    > = MutationCompendium::new();
+    let unmutated_genome: TestGenome = Genome::new(
+        GenomicInputSensor::default(),
+        Vec::new(),
+        vec![Gene::new(vec![TestInformation { value: 0 }])],
+    );
+    assert_eq!(compendium.size(), 0);
+    assert!(compendium.mutate(&unmutated_genome).is_none());
+}
+
+#[test]
+/// Tests if the function `add` of the [`MutationCompendium`] struct correctly adds [`Mutation`]s.
+fn test_mutation_compendium_add() {
+    let mut compendium: MutationCompendium<
+        TestReaction,
+        TestState,
+        TestInformation,
+        NoOpInputElement,
+        TestInput,
+    > = MutationCompendium::new();
+    assert_eq!(compendium.size(), 0);
+    compendium.add(Mutation::new(0.2, |genome: &TestGenome| Some(genome.duplicate())));
+    assert_eq!(compendium.size(), 1);
+    compendium.add(Mutation::new(0.3, |genome: &TestGenome| Some(genome.duplicate())));
+    assert_eq!(compendium.size(), 2);
+}
+
+#[test]
+/// Tests if the function `mutate` of the [`MutationCompendium`] struct correctly mutates a [`Genome`].
+fn test_mutation_compendium_mutate() {
+    {
+        // Zero percent chance mutations are never applied.
+        let compendium: MutationCompendium<
+            TestReaction,
+            TestState,
+            TestInformation,
+            NoOpInputElement,
+            TestInput,
+        > = vec![Mutation::new(0.0, |genome: &TestGenome| {
+            Some(genome.duplicate())
+        })]
+        .into();
+        let unmutated_genome: TestGenome = Genome::new(
+            GenomicInputSensor::default(),
+            Vec::new(),
+            vec![Gene::new(vec![TestInformation { value: 0 }])],
+        );
+        assert_eq!(compendium.size(), 1);
+        assert!(compendium.mutate(&unmutated_genome).is_none());
+    }
+    {
+        // 100 percent chance mutations are always applied.
+        let mutation = Mutation::new(1.0, |genome: &TestGenome| {
+            let mut mutated_genome = genome.duplicate();
+            let substrate = mutated_genome
+                .get_substrate_mut(GeneSubstrate::new(0, 0))
+                .unwrap();
+            *substrate = TestInformation {
+                value: substrate.value + 1,
+            };
+            Some(mutated_genome)
+        });
+        let compendium: MutationCompendium<
+            TestReaction,
+            TestState,
+            TestInformation,
+            NoOpInputElement,
+            TestInput,
+        > = vec![mutation].into();
+        let unmutated_genome: TestGenome = Genome::new(
+            GenomicInputSensor::default(),
+            Vec::new(),
+            vec![Gene::new(vec![TestInformation { value: 0 }])],
+        );
+        let expected_mutated_genome: TestGenome = Genome::new(
+            GenomicInputSensor::default(),
+            Vec::new(),
+            vec![Gene::new(vec![TestInformation {
+                value: MAX_MUTATION_EVENTS,
+            }])],
+        );
+        assert_eq!(compendium.size(), 1);
+        assert_eq!(Some(expected_mutated_genome), compendium.mutate(&unmutated_genome));
+    }
+    {
+        // Other mutations are always applied on a statistical basis.
+        let mutation_a = Mutation::new(0.1, |genome: &TestGenome| {
+            let mut mutated_genome = genome.duplicate();
+            let substrate = mutated_genome
+                .get_substrate_mut(GeneSubstrate::new(0, 0))
+                .unwrap();
+            if substrate.value < MAX_MUTATION_EVENTS {
+                *substrate = TestInformation {
+                    value: substrate.value + 1,
+                };
+            }
+            Some(mutated_genome)
+        });
+        let mutation_b = Mutation::new(0.2, |genome: &TestGenome| {
+            let mut mutated_genome = genome.duplicate();
+            let substrate = mutated_genome
+                .get_substrate_mut(GeneSubstrate::new(0, 0))
+                .unwrap();
+            if substrate.value < MAX_MUTATION_EVENTS {
+                *substrate = TestInformation {
+                    value: substrate.value + 1,
+                };
+            }
+            Some(mutated_genome)
+        });
+        let mutation_c = Mutation::new(0.3, |genome: &TestGenome| {
+            let mut mutated_genome = genome.duplicate();
+            let substrate = mutated_genome
+                .get_substrate_mut(GeneSubstrate::new(0, 0))
+                .unwrap();
+            if substrate.value < MAX_MUTATION_EVENTS {
+                *substrate = TestInformation {
+                    value: substrate.value + 1,
+                };
+            }
+            Some(mutated_genome)
+        });
+        let compendium: MutationCompendium<
+            TestReaction,
+            TestState,
+            TestInformation,
+            NoOpInputElement,
+            TestInput,
+        > = vec![mutation_a, mutation_b, mutation_c].into();
+        let unmutated_genome: TestGenome = Genome::new(
+            GenomicInputSensor::default(),
+            Vec::new(),
+            vec![Gene::new(vec![TestInformation { value: 0 }])],
+        );
+        assert_eq!(compendium.size(), 3);
+        if let Some(mutated_genome) = compendium.mutate(&unmutated_genome) {
+            assert!(
+                mutated_genome
+                    .get_substrate(GeneSubstrate::new(0, 0))
+                    .unwrap()
+                    .value
+                    <= MAX_MUTATION_EVENTS
+            );
+        }
+    }
 }
